d7305a372c0f34203b9e3cdeb91d819acfb708bf
carolinevannebo <carolinevannebo@icloud.com>
2026-01-17 15:08:04 +0100

    changed white color, border radius on global buttons, and center position on history page

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -9,18 +9,20 @@ function MainPage() {
             <div className="hero-content text-center">
                 <div className="max-w-md">
                     <h1 className="text-5xl font-bold">JavaZone 2026</h1>
-                    <div className="h-2" aria-hidden="true" />
+                    <div className="h-2" aria-hidden="true"/>
                     <h2 className="text-3xl font-bold mb-6">
                         September 2nd - 3rd, 2026, NOVA Spektrum, Lillestrøm
                     </h2>
-                    <p className={"text-2xl font-semibold"} >Ticket sales open March 2nd</p>
+                    <p className={"text-2xl font-semibold"}>Ticket sales open March 2nd</p>
 
-                    <div className="h-5" aria-hidden="true" />
+                    <div className="h-5" aria-hidden="true"/>
 
-                    <p className="text-2xl font-semibold">
-                        <a href={"/partner"}
-                           className="link link-primary text-black">Partner registration</a> open January 15th at 10:00
-                    </p>
+                    <div className="pt-4 pb-8">
+                        <a href="https://event.checkin.no/215047/javazone-2026-partnership"
+                           className="btn btn-primary btn-lg text-2xl px-12">
+                            Register as partner
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
