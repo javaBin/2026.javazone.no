5dd24fc528129434937b11fd85495d779b1b13a5
carolinevannebo <carolinevannebo@icloud.com>
2026-02-08 23:16:34 +0100

    merged dev into branch

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -14,13 +14,7 @@ const MainPage = () => {
           <p className={'text-xl md:text-2xl font-semibold center-text'}>Ticket sales open March 2nd</p>
 
           <LinkButton title="Register as partner" size="large" link="https://event.checkin.no/215047/javazone-2026-partnership" className="mt-6" />
-
-          <div className="pt-12 pb-8">
-            <p className="text-lg md:text-xl font-semibold center-text pb-4"> Want to volunteer at this years Javazone?</p>
-            <a href="/volunteer" className="btn btn-primary btn-md text-xl px-8">
-              Take a look here!
-            </a>
-          </div>
+          <LinkButton title="Sign up to volunteer" size="large" variant="primary-outline" link="/volunteer" className="mt-6" />
         </div>
       </div>
     </div>
