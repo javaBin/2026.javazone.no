e104d27bc929d8fc1d556d907667b0a91ad7d198
carolinevannebo <carolinevannebo@icloud.com>
2026-03-03 20:18:09 +0100

    scrolling fish is for test branch, not this PR

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,4 @@
 import BubbleSimple from '@/components/animated/bubbles/BubbleSimple.tsx'
-import ScrollingFish from '@/components/animated/scrollingFish/ScrollingFish.tsx'
 import Submarine from '@/components/animated/submarine/submarine.tsx'
 import WaveBackground from '@/components/animated/wave/WaveBackground.tsx'
 import Card from '@/components/Card.tsx'
@@ -9,4 +8,4 @@ import Header from '@/components/layout/Header.tsx'
 import LinkButton from '@/components/LinkButton.tsx'
 import Packages from '@/components/partners/Packages.tsx'
 
-export { BubbleSimple, Card, Footer, Header, Heading, LinkButton, Packages, ScrollingFish, Submarine, WaveBackground }
+export { BubbleSimple, Card, Footer, Header, Heading, LinkButton, Packages, Submarine, WaveBackground }
