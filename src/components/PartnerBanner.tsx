6849faa236583204ebfc3b1d109b3c7cb1ede1ed
Ellaoui94 <abdulquddous.abdellaoui@toll.no>
2026-06-02 19:17:07 +0200

    fix-partner-for-mobile-preview: Update PartnerBanner layout for improved responsiveness and aesthetics

--- src/components/PartnerBanner.tsx
+++ src/components/PartnerBanner.tsx
@@ -8,11 +8,16 @@ export const PartnerBanner = () => {
 
   return (
     <div className="block w-screen p-4 ml-[calc(-50vw+50%)] mr-[calc(-50vw+50%)] mt-4">
-      <div className="flex flex-wrap justify-center gap-10">
+      <div className="grid grid-cols-3 lg:grid-cols-6 gap-6 place-items-center">
         {shuffledPartners.map((partner) => (
           <HoverAnimation key={partner.name}>
             <a target="_blank" href={partner.homepageUrl} rel="noopener noreferrer">
-              <img src={partner.logoUrl} alt={partner.name} className="h-16 w-40 transform transition-transform hover:scale-125" loading="lazy" />
+              <img
+                src={partner.logoUrl}
+                alt={partner.name}
+                className="h-16 w-40 object-contain transition-transform hover:scale-125"
+                loading="lazy"
+              />
             </a>
           </HoverAnimation>
         ))}
